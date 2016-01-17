5e3c8b8494655078179562de679d29af96675c1f
Manisha Rana <manishar@sahajsoft.com>
2016-01-17 21:06:34 +0530

    adding name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116175821) do
+ActiveRecord::Schema.define(version: 20160117152739) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160116175821) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
